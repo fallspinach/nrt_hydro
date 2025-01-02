4c548a5b674ad712d2b0f5d9f6944edad4a700c5
Ming Pan <m3pan@ucsd.edu>
2025-01-01 20:24:47 -0800

    Re-factored run_nrt.py to run WRF-Hydro over the conus domain according to whether NLDAS-2 is available (xland=rfcs) or not (xland=hrrr).

--- scripts/wrf_hydro/run_nrt.py
+++ scripts/wrf_hydro/run_nrt.py
@@ -18,38 +18,17 @@ from datetime import datetime, timedelta
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'/utils')
 from utilities import config, find_last_time, replace_brackets
 
-## some setups
+## separate the WRF-Hydro simulation from post-processing
 
-## main function
-def main(argv):
+def run_wrf_hydro(domain, t1, t2, xland='rfcs', dep=-1):
 
-    '''main loop'''
-    curr_time = datetime.utcnow()
-    curr_time = curr_time.replace(tzinfo=pytz.utc)
-
-    if len(argv)>=1:
-        domain = argv[0]
-    else:
-        domain = 'cnrfc'
-    
     workdir = f'{config["base_dir"]}/wrf_hydro/{domain}/nrt/run'
-    
-    if len(argv)>=3:
-        t1 = datetime.strptime(argv[1], '%Y%m%d')
-        t2 = datetime.strptime(argv[2], '%Y%m%d')
-    else:
-        curr_day  = curr_time - timedelta(hours=curr_time.hour, minutes=curr_time.minute, seconds=curr_time.second, microseconds=curr_time.microsecond)
-        t1 = curr_day - timedelta(days=11)
-        t2 = find_last_time(f'{workdir}/../forcing/1km_hourly/202?/202?????.LDASIN_DOMAIN1', '%Y%m%d.LDASIN_DOMAIN1') - timedelta(days=1)
-        if t2 > curr_day - timedelta(days=1):
-            t2 = curr_day - timedelta(days=1)
-    
     ndays = (t2+timedelta(days=1)-t1).days
     nmons = round(ndays/30.5)
     if nmons==0:
         nmons = 1
 
-    print(f'Current day is {curr_time:%Y-%m-%d}, running model from {t1:%Y-%m-%d} to {t2:%Y-%m-%d} ({ndays:d} days).') 
+    print(f'Running model from {t1:%Y-%m-%d} to {t2:%Y-%m-%d} ({ndays:d} days).') 
 
     tpn = config['cores_per_node']
     config_dom = config['wrf_hydro'][domain]
@@ -85,7 +64,13 @@ def main(argv):
         
         if (not config_dom['lake']) and f=='hydro.namelist':
             replace_brackets(f, {'outlake  = 1': 'outlake  = 0', 'route_lake_f': '!route_lake_f'}, False)
-            
+
+        if domain=='conus' and f=='run_wrf_hydro.sh':
+            print(f'Adding cmd to run_wrf_hydro.sh to set XLAND to xland={xland}.')
+            target = f'{config["base_dir"]}/wrf_hydro/{domain}/domain/wrfinput_conus.nc'
+            cmd = f'ln -nfs wrfinput_{xland}_trim.nc {target}'
+            replace_brackets(f, {'module list': cmd}, False)
+
     # check restart files
     frestart = f'../restart/RESTART.{t1:%Y%m%d}00_DOMAIN1'
     if not os.path.isfile(frestart):
@@ -105,9 +90,61 @@ def main(argv):
             return 1
         t += timedelta(days=1)
 
-    ret = subprocess.check_output(['sbatch run_wrf_hydro.sh'], shell=True)
+    if dep==-1:
+        ret = subprocess.check_output([f'sbatch run_wrf_hydro.sh'], shell=True)
+    else:
+        ret = subprocess.check_output([f'sbatch -d afterok:{dep} run_wrf_hydro.sh'], shell=True)
     jid = ret.decode().split(' ')[-1].rstrip()
     print(f'WRF-Hydro job ID is: {jid}')
+
+    return jid
+
+
+## main function
+def main(argv):
+
+    '''main loop'''
+    curr_time = datetime.utcnow()
+    curr_time = curr_time.replace(tzinfo=pytz.utc)
+
+    print(f'Current day is {curr_time:%Y-%m-%d}.')
+
+    if len(argv)>=1:
+        domain = argv[0]
+    else:
+        domain = 'cnrfc'
+
+    workdir = f'{config["base_dir"]}/wrf_hydro/{domain}/nrt/run'
+        
+    if len(argv)>=3:
+        t1 = datetime.strptime(argv[1], '%Y%m%d')
+        t2 = datetime.strptime(argv[2], '%Y%m%d')
+    else:
+        curr_day  = curr_time - timedelta(hours=curr_time.hour, minutes=curr_time.minute, seconds=curr_time.second, microseconds=curr_time.microsecond)
+        t1 = curr_day - timedelta(days=11)
+        t2 = find_last_time(f'{workdir}/../forcing/1km_hourly/202?/202?????.LDASIN_DOMAIN1', '%Y%m%d.LDASIN_DOMAIN1') - timedelta(days=1)
+        if t2 > curr_day - timedelta(days=1):
+            t2 = curr_day - timedelta(days=1)
+            
+    t1 = t1.replace(tzinfo=pytz.utc)
+    t2 = t2.replace(tzinfo=pytz.utc)
+
+    config_dom = config['wrf_hydro'][domain]
+    
+    ndays = (t2+timedelta(days=1)-t1).days
+    nmons = round(ndays/30.5)
+    if nmons==0:
+        nmons = 1
+
+    nldas2_path = f'{config["base_dir"]}/forcing/nldas2/NLDAS_FORA0125_H.2.0' # path to NLDAS-2 archive folder
+    last_nldas2 = find_last_time(f'{nldas2_path}/202?/???/*.nc', 'NLDAS_FORA0125_H.A%Y%m%d.%H00.020.nc')
+
+    if domain=='conus' and t2>=last_nldas2:
+        print(f'CONUS domain and NLDAS-2 ends (on {last_nldas2:%Y-%m-%d}) earlier than {t2:%Y-%m-%d}. Split the simulation into two separate ones.')
+        jid = run_wrf_hydro(domain, t1,          last_nldas2-timedelta(days=1), xland='rfcs')
+        jid = run_wrf_hydro(domain, last_nldas2, t2,                            xland='hrrr', dep=jid)
+    else:
+        jid = run_wrf_hydro(domain, t1, t2)
     
     os.chdir(f'{config["base_dir"]}/scripts/wrf_hydro')
     nc_shared   = nmons
@@ -131,7 +168,7 @@ def main(argv):
     jid = ret.decode().split(' ')[-1].rstrip(); jid3 = jid
     print(f'Monthly plotting job ID is: {jid}')
     
-    if config_dom['lake'] and domain!='conus':
+    if domain in ['cnrfc']:
         
         cmd1 = f'unset SLURM_MEM_PER_NODE; python {config["base_dir"]}/scripts/wrf_hydro/extract_rivers_nrt.py'
         flog = f'{workdir}/log/extract_rivers_{t1:%Y%m}_{t2:%Y%m}.txt'
@@ -141,7 +178,7 @@ def main(argv):
         jid = ret.decode().split(' ')[-1].rstrip()
         print(f'River extraction will run with job ID: {jid}')
 
-    else:
+    elif domain in ['basins24']:
 
         cmd1 = f'unset SLURM_MEM_PER_NODE; mpirun -np 6 python {config["base_dir"]}/scripts/wrf_hydro/extract_average_nrt.py'
         flog = f'{workdir}/log/extract_averages_{t1:%Y%m}_{t2:%Y%m}.txt'
